c8ce4adc47f3ff25049428e032062990da69cacd
RisalAritma <99566612+RisalAritma@users.noreply.github.com>
2022-02-13 08:37:12 +0800

    Add files via upload

---
+++ 7Seg_Dasar/7Seg_Dasar.ino
@@ -0,0 +1,32 @@
+#define a 13
+#define b 12
+#define c 11
+#define d 10
+#define e 9
+#define f 8
+#define g 7
+
+
+void setup() {
+  pinMode(a,OUTPUT);
+  pinMode(b,OUTPUT);
+  pinMode(c,OUTPUT);
+  pinMode(d,OUTPUT);
+  pinMode(e,OUTPUT);
+  pinMode(f,OUTPUT);
+  pinMode(g,OUTPUT);
+
+  digitalWrite(a,HIGH);
+  digitalWrite(b,LOW);
+  digitalWrite(c,LOW);
+  digitalWrite(d,HIGH);
+  digitalWrite(e,HIGH);
+  digitalWrite(f,LOW);
+  digitalWrite(g,LOW);
+
+}
+
+void loop() {
+  
+
+}
