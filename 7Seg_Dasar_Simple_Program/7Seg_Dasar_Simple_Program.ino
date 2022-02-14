10e82531eca961adae4a4bb4eb498794a54bb173
Ahmad Risal <99566612+RisalAritma@users.noreply.github.com>
2022-02-14 09:01:39 +0800

    Add files via upload

---
+++ 7Seg_Dasar_Simple_Program/7Seg_Dasar_Simple_Program.ino
@@ -0,0 +1,29 @@
+#define a 13
+#define b 12
+#define c 11
+#define d 10
+#define e 9
+#define f 8
+#define g 7
+
+void setup() {
+  pinMode(a,OUTPUT);
+  pinMode(b,OUTPUT);
+  pinMode(c,OUTPUT);
+  pinMode(d,OUTPUT);
+  pinMode(e,OUTPUT);
+  pinMode(f,OUTPUT);
+  pinMode(g,OUTPUT);
+}
+
+void loop() {
+  angka(0);
+  delay(1000);
+
+  angka(1);
+  delay(1000);
+
+  angka(2);
+  delay(1000);
+  
+}
